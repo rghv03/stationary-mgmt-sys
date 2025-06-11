6d40a1c0fdbb45f87a23de5e90d4c65f6480e624
Raghav Shandilya <raghavshandilya@Raghavs-MacBook-Air.local>
2025-06-11 18:47:36 +0530

    done

--- app.py
+++ app.py
@@ -45,6 +45,29 @@ def login():
 def logout():
     session.clear()
     return redirect('/login')
+#change password route
+@app.route('/change_password', methods = ['POST','GET'])
+def change_password():
+    if 'user_id' not in session:
+        return redirect('/signup')
+    user = User.query.get(session['user_id'])
+    error = None
+    if request.method == 'POST':
+        old_password = request.form['old_password']
+        new_password = request.form['new_password']
+        confirm_password = request.form['confirm_password']
+    
+        if not check_password_hash(user.password , old_password):
+            error = "Old password is Incorrect!!!"
+        elif new_password !=confirm_password:
+            error = "New passwords do not match"
+        else:
+            user.password = generate_password_hash(new_password)
+            db.session.commit()
+            flash("Password Updated successfully.")
+            return redirect('/change_password')
+    return render_template('change_password.html', error = error , role = session.get('role'))
+
 
 #Dashboard route
 @app.route('/Dashboard')
